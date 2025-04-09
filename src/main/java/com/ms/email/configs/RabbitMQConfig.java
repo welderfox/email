59582ca61d339b7feb9b64f21aa4ef378607f7e7
welder <welderfox@hotmail.com>
2025-04-09 11:52:02 -0300

    #Ajuste e correcao de import

--- src/main/java/com/ms/email/configs/RabbitMQConfig.java
+++ src/main/java/com/ms/email/configs/RabbitMQConfig.java
@@ -1,6 +1,5 @@
 package com.ms.email.configs;
 
-
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.beans.factory.annotation.Value;
@@ -9,19 +8,21 @@ import org.springframework.context.annotation.Configuration;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-
-
 @Configuration
 public class RabbitMQConfig {
-	
+	// Configuração do RabbitMQ
+	// Queue para EmailRecordDTO
 	@Value("${broker.queue.email.name}")
 	private String queue;
-	
+
 	@Bean
 	public Queue queue() {
 		return new Queue(queue, true);
-		}
-	
+	}
+
+	// Converter para EmailRecordDTo
+	// Configuração do Jackson2JsonMessageConverter
+	// Converter para EmailRecordDTO
 	@Bean
 	public Jackson2JsonMessageConverter messageConverter() {
 		ObjectMapper objectMapper = new ObjectMapper();
--- src/main/java/com/ms/email/dtos/EmailRecordDto.java
+++ src/main/java/com/ms/email/dtos/EmailRecordDto.java
@@ -1,7 +1,5 @@
 package com.ms.email.dtos;
 
-import org.hibernate.validator.constraints.UUID;
+import java.util.UUID;
 
-public record EmailRecordDto(UUID userId, String emailTo,String subject, String text) {
-	
-}
+public record EmailRecordDto(UUID userId, String emailTo,String subject, String text) {}
